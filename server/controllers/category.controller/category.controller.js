751abdae787effe520ca889add644e35eba78046
email <phamson061297@gmail.com>
2022-03-06 07:53:41 -0400

    create Category schema, and category routes

---
+++ server/controllers/category.controller/category.controller.js
@@ -0,0 +1,7 @@
+const Category = require("./../../models/category.schema");
+
+const createCategory = async (req, res) => {};
+const getAllCategory = async (req, res) => {};
+const getOneCategory = async (req, res) => {};
+const updateCategory = async (req, res) => {};
+const deleteCategory = async (req, res) => {};
---
+++ server/routes/category/category.route.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+//middlewares
+const {
+  authTokenCheck,
+  adminCheck,
+} = require("../../middlewares/authentication-middleware/authentication.middleware");
+
+//controller
+const categoryController = require("./../../controllers/category.controller/category.controller");
+
+//routes
+router.route("/categories").get(categoryController.getAllCategory);
+router.route("/category/:slug").get(categoryController.getOneCategory);
+router
+  .route("/category")
+  .create(authTokenCheck, adminCheck, categoryController.createCategory);
+router
+  .route("/category/:slug")
+  .put(authTokenCheck, adminCheck, categoryController.updateCategory);
+router
+  .route("/category/:slug")
+  .delete(authTokenCheck, adminCheck, categoryController.deleteCategory);
+
+module.exports = router;
