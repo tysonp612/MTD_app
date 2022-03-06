c9c27e0318c9a59a488c3dc95c8249a4b37d62df
email <phamson061297@gmail.com>
2022-03-06 19:35:51 -0400

    Create CRUD utils to make request from front end to back end with axios

---
+++ client/src/utils/category/category.utils.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const getCategories = async () => {
+  return await axios.get(`${process.env.REACT_APP_API}/categories`);
+};
+
+export const getOneCategory = async (slug) => {
+  return await axios.get(`${process.env.REACT_APP_API}/categories/${slug}`);
+};
+
+//send token in header
+export const removeCategory = async (slug, authtoken) => {
+  return (
+    await axios.delete(`${process.env.REACT_APP_API}/categories/${slug}`),
+    { headers: { authtoken } }
+  );
+};
+
+export const updateCategory = async (slug, authtoken, updateCategory) => {
+  return (
+    await axios.put(`${process.env.REACT_APP_API}/categories/${slug}`),
+    updateCategory,
+    { headers: { authtoken } }
+  );
+};
+
+export const createCategory = async (slug, authtoken, createCategory) => {
+  return (
+    await axios.put(`${process.env.REACT_APP_API}/categories/${slug}`),
+    createCategory,
+    { headers: { authtoken } }
+  );
+};
