5b9e000d5967501736a72d708a4203aaaa5dacd8
email <phamson061297@gmail.com>
2022-03-12 08:11:59 -0400

    changing file direction

--- client/server/firebase/index.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./../config/firebaseServiceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
--- client/server/routes/user/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/user", (req, res) => {
-  res.json({ data: "hey from user api" });
-});
-
-module.exports = router;
