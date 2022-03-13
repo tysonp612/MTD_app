409bb40443f4bd73e5b00418878644c7845992ac
email <phamson061297@gmail.com>
2022-03-13 15:33:53 -0300

    Adding product schema

---
+++ server/models/product.schema.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      minlength: 2,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 200,
+      text: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+    },
+    subcategory: [{ type: ObjectID, ref: "SubCategory" }],
+    quantity: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: String,
+      enum: ["Yes", "No"],
+    },
+    color: {
+      type: String,
+      enum: ["Black", "Silver", "White", "Blue", "Dark Grey"],
+    },
+    brand: {
+      type: String,
+    },
+    // ratings: [
+    //   {
+    //     star: Number,
+    //     postedBy: {
+    //       type: ObjectId,
+    //       ref: "User",
+    //     },
+    //   },
+    // ],
+  },
+  { timestamps: true }
+);
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
