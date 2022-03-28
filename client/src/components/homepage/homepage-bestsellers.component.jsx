5fd3558c00f0439786d3c9543195877658c28ca4
email <phamson061297@gmail.com>
2022-03-28 15:26:15 -0300

    adding pagination for best sellers

--- client/src/components/homepage/homepage-bestsellers.component.jsx
+++ client/src/components/homepage/homepage-bestsellers.component.jsx
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { getSortedProducts } from "./../../utils/products/products.utils";
+import {
+  productsCount,
+  getSortedProducts,
+} from "./../../utils/products/products.utils";
 import { LoadingCard } from "./../card/loading-card.component";
 import { ProductCard } from "./../card/regular.product-card.component";
+import { Pagination } from "antd";
 export const BestSellers = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [productsQuantity, setProductsQuantity] = useState(0);
   useEffect(() => {
     loadProducts();
-  }, []);
+    loadProductsCount();
+  }, [page]);
   const loadProducts = async () => {
     setLoading(true);
-    await getSortedProducts("sold", "desc", 1)
+    await getSortedProducts("sold", "desc", page)
       .then((res) => {
         setProducts(res.data);
         setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
+  const loadProductsCount = async () => {
+    await productsCount()
+      .then((res) => {
+        console.log(res.data);
+        setProductsQuantity(res.data);
       })
       .catch((err) => console.log(err));
   };
@@ -30,6 +45,14 @@ export const BestSellers = () => {
               </div>
             );
           })}
+          <Pagination
+            className="text-center pt-3"
+            current={page}
+            total={Math.round((productsQuantity / 3) * 10)}
+            onChange={(value) => {
+              setPage(value);
+            }}
+          />
         </div>
       )}
     </div>
