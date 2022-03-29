e83835824968ea63826d6363f8ef20e37eb642a2
email <phamson061297@gmail.com>
2022-03-29 11:18:54 -0300

    adding carousel to single product page

--- client/src/components/card/single-product-card.component.jsx
+++ client/src/components/card/single-product-card.component.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { Card } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 const { Meta } = Card;
 export const SingleProduct = ({ product }) => {
   const { title, description, images, slug } = product;
   return (
     <>
-      <div className="col-md-7">Image Carousel</div>
+      <div className="col-md-7">
+        <Carousel showArrows={true} infiniteLoop>
+          {images &&
+            images.map((images) => {
+              return <img src={images.url} key={images.public_id} />;
+            })}
+        </Carousel>
+      </div>
       <div className="col-md-5">
         <Card
           actions={[
