3a7308bfb887180c2b9c466f0882fc7e4be91391
email <phamson061297@gmail.com>
2022-04-23 23:30:57 -0300

    Adding feature of decreasing quantity and increasing sold by updating Product schema in backend

--- server/controllers/order.controller/order.controller.js
+++ server/controllers/order.controller/order.controller.js
@@ -9,12 +9,21 @@ exports.createOrder = async (req, res) => {
     const { paymentIntent } = req.body;
     const user = await User.findOne({ email: req.user.email });
     const { products } = await Cart.findOne({ orderedBy: user._id });
+    console.log(products);
+    products.forEach(async (prod) => {
+      const updatedProduct = await Product.findByIdAndUpdate(
+        prod.product,
+        {
+          $inc: { quantity: -prod.cartQuantity, sold: prod.cartQuantity },
+        },
+        { new: true }
+      );
+    });
     const newOrder = await new Order({
       products,
       paymentIntent,
       orderedBy: user._id,
     }).save();
-    console.log(newOrder);
     res.status(200).json({ ok: true });
   } catch (err) {
     console.log(err);
