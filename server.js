d96907668e4e78f0186a75556fa52f602f07354b
email <phamson061297@gmail.com>
2022-04-30 21:35:14 -0300

    heroku deploy 19

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+require("dotenv").config();
+
+//import routes
+const authRoutes = require("./server/routes/authentication/authentication.route");
+//1) CREATE APP FROM EXPRESS AND SET UP MONGOOSE CONFIG
+//app
+const app = express();
+
+//as we connect, we got a promise back
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(`DB connection error ${err}`));
+
+//2 SETTING UP MIDDLEWARE
+//middleware
+// It simplifies the process of logging requests to your application. You might think of Morgan as a helper that generates request logs
+app.use(morgan("dev"));
+//helps communicate between front and back end, parse data under 2mb
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//overcome CORS error
+app.use(cors());
+
+//routes MIDDLEWARE
+// app.use("/api", authRoutes);
+
+//this line below is for calling app.use for all the routes as the app starts, so we don;t have to call each route manually
+fs.readdirSync("./routes").map((r) =>
+  app.use(require(`./routes/${r}/${r}.route`))
+);
+
+app.use(express.static(path.join(__dirname + "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build", "index.html"));
+});
+
+//3 APP LISTENS TO PORT
+//port
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log("listen to server running "));
+
+// const proxy = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   // add other server routes to path array
+//   app.use(proxy(["/api"], { target: "http://localhost:5000" }));
+// };
