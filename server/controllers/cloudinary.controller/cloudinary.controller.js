54f54005d83ad82b2072c3c145facdd7651b3d34
Thai Son Pham <th811292@dal.ca>
2024-11-08 16:50:39 +0100

    fix: clean code for category controller and cloudinary controler

--- server/controllers/cloudinary.controller/cloudinary.controller.js
+++ server/controllers/cloudinary.controller/cloudinary.controller.js
@@ -2,31 +2,42 @@ const Product = require("./../../models/product.schema");
 const cloudinary = require("cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 exports.uploadImages = async (req, res) => {
-  try {
-    let result = await cloudinary.uploader.upload(req.body.images, {
-      public_id: `${Date.now()}`,
-      resource_type: "auto",
-    });
-    res.status(200).json({
-      public_id: result.public_id,
-      url: result.secure_url,
-    });
-  } catch (err) {
-    console.log(err);
-  }
+	const { images } = req.body;
+	if (!images) {
+		return res.status(400).json({ error: "No images provided!" });
+	}
+
+	try {
+		let result = await cloudinary.uploader.upload(images, {
+			public_id: `${Date.now()}`,
+			resource_type: "auto",
+		});
+		res.status(200).json({
+			public_id: result.public_id,
+			url: result.secure_url,
+		});
+	} catch (error) {
+		console.error("Error uploading image:", err);
+		res.status(500).json({ error: "Failed to upload image" });
+	}
 };
 exports.removeImages = async (req, res) => {
-  let image_id = req.body.public_id;
+	const { public_id: imageId } = req.body;
+	// Check if image ID is provided
+	if (!imageId) {
+		return res.status(400).json({ error: "No image ID provided" });
+	}
 
-  try {
-    await cloudinary.uploader.destroy(image_id);
-    res.status(200).send("image deleted");
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		await cloudinary.uploader.destroy(imageId);
+		res.status(200).json({ message: "Image deleted successfully" });
+	} catch (err) {
+		console.error("Error deleting image:", err);
+		res.status(500).json({ error: "Failed to delete image" });
+	}
 };
